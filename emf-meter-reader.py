238d0dbcc1d4e0282376a01e76de0556c1dc385d
Chris Elsmore <elsmorian@gmail.com>
2014-08-14 22:53:44 +0100

    fleshed out CSV logger

--- emf-meter-reader.py
+++ emf-meter-reader.py
@@ -2,9 +2,13 @@
 # -*- coding: utf-8 -*-
 import argparse
 import sys
+import csv
+import time
+import os.path
 from datetime import datetime
 
 from Rail350V import Rail350V
+
 
 class MeterReader(object):
 
@@ -35,20 +39,42 @@ class MeterReader(object):
         return aquired_data
 
 
+class MeterCSVLogger(object):
+
+    def __init(self, csv_file):
+        self.CSV_HEADERS = ['channel', 'timestamp', 'value']
+        self.file = csv_file
+        if not os.path.isfile(self.file):
+            with open(self.file, 'wb') as fp:
+                writer = csv.writer(fp)
+                writer.write(self.CSV_HEADERS)
+
+    def write_meter_data(self, meter_data):
+        with open(self.file, 'wb') as fp:
+            writer = csv.writer(fp)
+
+            for channel, datum in meter_data.iteritems():
+                row = [channel, datum['timestamp'], datum['value']]
+                writer.write(row)
+
+
 def main():
     try:
-    	parser = argparse.ArgumentParser()
-    	parser.add_argument("generator_name", help="name of the generator")
-    	parser.add_argument("serial_port", help="serial port")
-    	parser.add_argument("csv_file", help="csv log file location")
+        parser = argparse.ArgumentParser()
+        parser.add_argument("generator_name", help="name of the generator")
+        parser.add_argument("serial_port", help="serial port")
+        parser.add_argument("csv_file", help="csv log file location")
         args = parser.parse_args()
 
         reader = MeterReader(args.generator_name, args.serial_port)
+        csv_logger = MeterCSVLogger(args.csv_file)
 
-        for k, v in reader.aquire_data().iteritems():
-            print k
-            print v
-            print "****"
+        while True:
+            d = reader.aquire_data()
+            print d
+            print "**********"
+            csv_logger.write_meter_data(d)
+            time.sleep(5)
 
     except KeyboardInterrupt:
         print "Shutdown requested...exiting"
